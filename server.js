8aa7f91405ca30c8cd3f2ce2a50605f62892f5b9
claudiabringaseverett <claudiaeverett9@gmail.com>
2023-07-27 14:56:02 +0000

    updating

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const mockEvents = {
     events: [
         { title: 'an event', id: 1, description: 'something really cool' },
         { title: 'another event', id: 2, description: 'something even cooler' },
-	{title: 'another event', id: 3, description: 'something else'}
+	{ title:  'one more event', id: 3, description: 'something else' }
     ]
 };
 
